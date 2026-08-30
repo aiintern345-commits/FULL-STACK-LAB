be10615cf0b2670276cd62d47619d7ed4109a260
aiintern345-commits <aiintern345@gmail.com>
2026-08-30 23:13:09 +0530

    Update Hybrid_inheritance.js

--- Assigments/assignment-1/Hybrid_inheritance.js
+++ Assigments/assignment-1/Hybrid_inheritance.js
@@ -1,1 +1,36 @@
+//Hybrid inheritance is a combination of two or more types of inheritance.
+// Since JavaScript does not directly support multiple inheritance with classes, we can use mixins.
+class Grandparent {
+  showGrandparent() {
+    console.log("This is Grandparent");
+  }
+}
 
+class Parent extends Grandparent {
+  showParent() {
+    console.log("This is Parent");
+  }
+}
+
+class Child1 extends Parent {
+  showChild1() {
+    console.log("This is Child 1");
+  }
+}
+
+class Child2 extends Parent {
+  showChild2() {
+    console.log("This is Child 2");
+  }
+}
+
+let child1 = new Child1();
+let child2 = new Child2();
+
+child1.showGrandparent();
+child1.showParent();
+child1.showChild1();
+
+child2.showGrandparent();
+child2.showParent();
+child2.showChild2();
